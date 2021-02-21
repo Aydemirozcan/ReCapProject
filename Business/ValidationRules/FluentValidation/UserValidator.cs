d764473ac86ef869c2a307efad53f9dc14fb4aa0
Aydemir Özcan <aydemir_kanarya@hotmail.com>
2021-02-21 18:51:15 +0300

    Updated Validation And AOP Support (21.02.2021)

--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -14,20 +14,9 @@ namespace Business.ValidationRules.FluentValidation
         public UserValidator()
         {
             RuleFor(u => u.Id).GreaterThanOrEqualTo(8);
-            RuleFor(u => u.Email).Must(CheckEmail).WithMessage("This e-mail belongs to another user.You must write different e-mail");
+
         }
 
-        private bool CheckEmail(string arg)
-        {
-            IUserDal userDal = new EfUserDal();
-            var check = userDal.Get(u => u.Email == arg);
-
-            if (check ==null)
-            {
-                return true;
-            }
-            return false;        
-        
-        }
+      
     }
 }
